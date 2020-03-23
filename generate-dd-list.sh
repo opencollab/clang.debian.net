984e5f339c597070763e4d8768597937115eb45d
Sylvestre Ledru <sledru@mozilla.com>
2020-03-23 22:02:49 +0100

    add a script to generate the list of maintainers

---
+++ generate-dd-list.sh
@@ -0,0 +1,13 @@
+# Ugly script to create the maintainers.txt file with links
+# usage:
+# bash -v generate-dd-list.sh 9.0.1 2020-02-24
+VERSION=$1
+DATE=$2
+dd-list $(awk '{print $1}' scanlog-$VERSION-$DATE) > maintainers.txt
+sed -i -e "s| (U)$||g" maintainers.txt
+LIST=$(grep "   " maintainers.txt)
+for PKG in $LIST; do
+    V=$(grep "^$PKG " scanlog-$VERSION-$DATE|awk '{print $2}')
+    sed -i -e "s|^   $PKG\$|   <a href='/logs/$DATE-$VERSION/${PKG}_${V}_unstable_clang${VERSION}.log'>$PKG</a>|g" maintainers.txt
+done
+
