19da4a7009d63ae8b92de7d3d02f7748bde7ad76
Sylvestre Ledru <sylvestre@debian.org>
2020-10-31 00:02:50 +0100

    update of the script with more recent data

--- insert.php
+++ insert.php
@@ -8,13 +8,13 @@ include "listErrors.php";
 if (isset($argv[1])) {
   $CLANG_VERSION=$argv[1];
 } else {
-  $CLANG_VERSION="7";
+  $CLANG_VERSION="11";
 }
 
 if (isset($argv[2])) {
   $DATE_REBUILD=$argv[2];
 } else {
-  $DATE_REBUILD="2019-01-01";
+  $DATE_REBUILD="2020-10-30";
 }
 
 $QUERY="DELETE FROM errors WHERE clang_version='$CLANG_VERSION' AND date_build='$DATE_REBUILD 00:00:00'";
